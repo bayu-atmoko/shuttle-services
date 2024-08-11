c8026f93696270fdcc247a763560756b27a4df9c
bayu-surya-atmoko <bayu.surya@gits.id>
2024-08-11 10:45:04 +0700

    feat(map): add locator for injection

--- features/route/lib/locator.dart
+++ features/route/lib/locator.dart
@@ -1,11 +1,18 @@
 //
 // Generated file. Edit just you manually add or delete a page.
 //
-
-import 'package:route/map/locator.dart';
-import 'package:route/maps_place_picker/locator.dart';
+import 'route_recommendation/locator.dart';
+import 'detail_route/locator.dart';
+import 'list_route/locator.dart';
+import 'map/locator.dart';
+import 'maps_place_picker/locator.dart';
+import 'detail_maps/locator.dart';
 
 void setupLocatorFeatureRoute() {
+  setupLocatorRouteRecommendation();
+  setupLocatorDetailRoute();
+  setupLocatorListRoute();
   setupLocatorMap();
   setupLocatorMapsPlacePicker();
+  setupLocatorDetailMaps();
 }
